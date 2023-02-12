c897b69bea08a215dfd4bd609ecf25878d1cb3be
league <league@imac-24.attlocal.net>
2023-02-11 16:31:21 -0800

    look over everthing and do

--- src/_04_backpack/BackpackRunner.java
+++ src/_04_backpack/BackpackRunner.java
@@ -2,7 +2,9 @@ package _04_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
-		new Backpack().packAndCheck();
+		Backpack backpack = new Backpack();
+		backpack.packAndCheck();
+		
 		
 	}
 }
